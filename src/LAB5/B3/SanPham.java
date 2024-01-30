f9e2995d9b0424b2ab7e88791b4b23c1a6a6d88b
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-01-30 15:47:53 +0700

    upload

---
+++ src/LAB5/B3/SanPham.java
@@ -0,0 +1,20 @@
+package LAB5.B3;
+
+public class SanPham {
+    String ten;
+    double gia;
+    public SanPham(){}
+
+    public SanPham(String ten, double gia) {
+        this.ten = ten;
+        this.gia = gia;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+}

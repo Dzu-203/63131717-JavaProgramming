d3f37cf7bd4644957470c9b505cdea7a0338b31d
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-02-28 09:28:21 +0700

    upload

---
+++ src/TH_Bai1_Login/ChuongTrinhDangNhap.java
@@ -0,0 +1,10 @@
+package TH_Bai1_Login;
+
+public class ChuongTrinhDangNhap {
+
+	public static void main(String[] args) {
+		LoginFrame loginPage = new LoginFrame();
+		loginPage.setVisible(true);
+	}
+
+}
---
+++ src/TH_Bai2_PTBac2/ChuongTrinhBac2.java
@@ -0,0 +1,8 @@
+package TH_Bai2_PTBac2;
+
+public class ChuongTrinhBac2 {
+	public static void main(String[] args) {
+		new ManHinhPhuongTrinhBac2().setVisible(true);
+	}
+
+}
